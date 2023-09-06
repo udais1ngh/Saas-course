1c9350772a58e6f90678484778f854ec866ec250
udais1ngh <udaisingh3520@gmail.com>
2023-09-06 21:27:24 +0530

    3rd try

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -30,7 +30,7 @@ export default function ProductPage({ prodeuct }) {
         ProductContent();
 
 
-    }, [])
+    })
 
 
     return (
--- src/pages/success.js
+++ src/pages/success.js
@@ -5,7 +5,7 @@ export default function SuccessPage() {
     return (
         <div className="h-screen">
             <div className=" flex flex-col justify-center items-center h-full">
-                <h1 className="text-3xl font-bold">You're in! <br /></h1>
+                <h1 className="text-3xl font-bold">You&apos;re in! <br /></h1>
                 <Link href="/login">
                     <button className="text-black m-1    bg-white   hover:bg-slate-300 focus:outline-none
                 font-medium  border-2 border-black p-2 text-sm px-5 py-2.5 text-center  " >Go to login</button>
